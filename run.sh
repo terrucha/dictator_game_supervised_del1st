fc368bcf2e02e7f179fad2b8f818715264a3f52b
terrucha <terrucha@gmail.com>
2025-07-24 09:51:17 +0200

    second deploy?

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export DATABASE_URL=postgresql://uwags5vdnxhen9una2pd:24DjvzLdhpacZRX476js@bue4eptukscbmmcgfa6g-postgresql.services.clever-cloud.com:7536/bue4eptukscbmmcgfa6g
+export DATABASE_URL=${POSTGRESQL_ADDON_URI}
 otree prodserver 9000
